e9c2e3f0241ad2ceb0e149d57480c65787f244de
mpeirone <m.peirone.9953@vallauri.edu>
2021-06-24 20:55:34 +0200

    Create OpenMediaVault.sh

---
+++ OpenMediaVault.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#root check
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+#install from https://github.com/OpenMediaVault-Plugin-Developers/installScript
+wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
