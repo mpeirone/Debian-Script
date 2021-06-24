a15e435aaa980725cf1ad678866d7d7bfa1c8497
mpeirone <m.peirone.9953@vallauri.edu>
2021-06-24 21:13:56 +0200

    Update and rename cockpit to cockpit.sh

--- cockpit.sh
+++ cockpit.sh
@@ -7,6 +7,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 #thanks to https://cockpit-project.org/running#debian
+
 echo 'deb http://deb.debian.org/debian buster-backports main' > /etc/apt/sources.list.d/backports.list
 apt update
 
