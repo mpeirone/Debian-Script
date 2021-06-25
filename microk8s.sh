16fffc090504d4c64e8cfa2bbcd64999a5d0d975
mpeirone <m.peirone.9953@vallauri.edu>
2021-06-25 16:21:27 +0200

    Update microk8s.sh

--- microk8s.sh
+++ microk8s.sh
@@ -9,4 +9,8 @@ fi
 #thanks to https://microk8s.io/
 apt update
 apt install snapd -yy
+echo 'export PATH="$PATH:/snap/bin"'  >> ~/.bashrc
 snap install microk8s --classic
+echo "alias kubectl='microk8s kubectl'"  >> ~/.bash_aliases
+
+sudo usermod -a -G microk8s $(whoami)
