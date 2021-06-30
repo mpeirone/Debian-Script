0bd84410e3730472ce581f523f6fdede72e3d6d5
mpeirone <m.peirone.9953@vallauri.edu>
2021-06-30 19:28:24 +0200

    Create microk8s_cust.sh

---
+++ microk8s_cust.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#not root check
+if [[ $EUID -e 0 ]]; then
+   echo "This script must be run as not root" 
+   exit 1
+fi
+
+echo "alias kubectl='microk8s kubectl'"  >> ~/.bash_aliases
+sudo usermod -a -G microk8s $(whoami)
+
+echo "now reboot"
