7d0ea475874ef1ee5174f91cf73f135ded5a7c76
mpeirone <mpcom2000@gmail.com>
2021-11-17 20:59:39 +0100

    Update rtl_tcp_service.sh

--- rtl_tcp_service.sh
+++ rtl_tcp_service.sh
@@ -1,11 +1,18 @@
 #/bin/bash
 
-echo $$ > .rtl_tcp_script_pid
-while [[ $(< .rtl_tcp_script_pid) == $$ ]]
+path="/home/mpcom/scripts"
+
+
+
+echo $$ > "${path}/.rtl_tcp_script_pid"
+while [[ $(< "${path}/.rtl_tcp_script_pid") == $$ ]]
 do
-        procNum=$(ps -ef | grep 'rtl_tcp' | wc -l)
-        pkill -9 -f rtl_tcp
-        echo "Killed $procNum processes, starting rtl_tcp"
+        procNum=$(ps -ef | grep 'rtl_tcp' | egrep -v "bash|grep" |  cut -b11-15)
+        if [ $(echo $procNum | wc -w) -gt 0 ]
+        then
+                echo "Killed $(echo $procNum | wc -w) processes, starting rtl_tcp"
+                kill -9 $procNum
+        fi
         rtl_tcp -a 0.0.0.0
         sleep 1
 done
